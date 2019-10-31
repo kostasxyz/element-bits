380da8817f6ea8f3734d72d348f4b31b8f3c2caa
Kostas Charal <skapator@gmail.com>
2019-10-31 13:42:07 +0200

    Pickadate addition

--- element-bits.php
+++ element-bits.php
@@ -103,10 +103,10 @@ function element_bits_init() {
 
     // Front end scripts/styles
     add_action( 'wp_enqueue_scripts', function() {
-        wp_enqueue_style( 'pickadate', ELBITS_URL . 'assets/vendor/pickadate/lib/compressed/themes/default.css', [], ELBITS_VERSION );
-        wp_enqueue_style( 'pickadate-date', ELBITS_URL . 'assets/vendor/pickadate/lib/compressed/themes/default.date.css', [], ELBITS_VERSION );
-        wp_enqueue_script( 'pickadate', ELBITS_URL . 'assets/vendor/pickadate/lib/compressed/picker.js', [ 'jquery' ], ELBITS_VERSION, true );
-        wp_enqueue_script( 'pickadate-date', ELBITS_URL . 'assets/vendor/pickadate/lib/compressed/picker.date.js', [ 'jquery' ], ELBITS_VERSION, true );
+        wp_enqueue_style( 'pickadate', ELBITS_URL . 'assets/vendor/pickadate/themes/default.css', [], ELBITS_VERSION );
+        wp_enqueue_style( 'pickadate-date', ELBITS_URL . 'assets/vendor/pickadate/themes/default.date.css', [], ELBITS_VERSION );
+        wp_enqueue_script( 'pickadate', ELBITS_URL . 'assets/vendor/pickadate/picker.js', [ 'jquery' ], ELBITS_VERSION, true );
+        wp_enqueue_script( 'pickadate-date', ELBITS_URL . 'assets/vendor/pickadate/picker.date.js', [ 'jquery' ], ELBITS_VERSION, true );
 
 		wp_enqueue_style( 'element-bits', ELBITS_URL . 'assets/css/element-bits.css', [], ELBITS_VERSION );
         wp_enqueue_script( 'element-bits', ELBITS_URL . 'assets/js/element-bits.js', [ 'jquery' ], ELBITS_VERSION, true );
