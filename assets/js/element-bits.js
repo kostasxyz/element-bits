cb150fdcd7feb3950c2d0677d4d56efec66cd0bd
Kostas.ch <skapator@gmail.com>
2020-12-22 12:43:25 +0200

    Close elementor menu popup on anchor link click

--- assets/js/element-bits.js
+++ assets/js/element-bits.js
@@ -2,10 +2,16 @@
     //-----------------------------------------------------
     // Menu icon
 
-  $('body').on( 'click', '.ebjs-menu-icon-toggle', function(e) {
+  $(document).on( 'click', '.ebjs-menu-icon-toggle', function(e) {
     e.preventDefault();
     // deprecated css class
     $('body').toggleClass('eb-menu-icon-btn--toggled');
+    $('body').toggleClass('eb-menu-icon-btn--active');
+  });
+
+  // Close popuop with nav links with hashtags
+  $(document).on('click','.ntr-nav-link-anchor', function(event){
+    elementorProFrontend.modules.popup.closePopup( {}, event);
     $('body').toggleClass('eb-menu-icon-btn--active');
   });
 
