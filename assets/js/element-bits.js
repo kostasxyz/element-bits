78fa63139e036ae36cf40cbc68fd5c7cefb7ed27
Kostas.ch <skapator@gmail.com>
2021-09-24 14:59:45 +0100

    Hahsbang and has children menu links fix

--- assets/js/element-bits.js
+++ assets/js/element-bits.js
@@ -1,6 +1,6 @@
 (function($){
-    //-----------------------------------------------------
-    // Menu icon
+  //-----------------------------------------------------
+  // Menu icon
 
   $(document).on( 'click', '.ebjs-menu-icon-toggle', function(e) {
     e.preventDefault();
@@ -9,8 +9,9 @@
     $('body').toggleClass('eb-menu-icon-btn--active');
   });
 
+  //-----------------------------------------------------
   // Close popuop with nav links with hashtags
-  $(document).on('click','.ntr-nav-link-anchor', function(event){
+  $(document).on('click','.ntr-nav-item:not(.menu-item-has-children ) .ntr-nav-link-anchor', function(event){
     elementorProFrontend.modules.popup.closePopup( {}, event);
     $('body').toggleClass('eb-menu-icon-btn--active');
   });
