927642c47e3bef7f27dd14325fc2244a2dffe2ce
Kostas Charalampidis <xyzkostas@gmail.com>
2024-12-18 12:50:36 +0000

    fix accordion

--- assets/js/eb-accordion-menu.js
+++ assets/js/eb-accordion-menu.js
@@ -1,59 +1,49 @@
-;(function($) {
-    'use strict';
+;( function($) {
+  /**
+   * @param $scope The Widget wrapper element as a jQuery element
+   * @param $ The jQuery alias
+   */
 
-    var ebAccordionMenuWidget = function($scope, $) {
-        var $menu = $scope.find('ul.menu');
-        var $parents = $menu.find('li.menu-item-has-children');
-        var $submenus = $parents.find('ul.sub-menu');
+  var widget = function( $scope, $ ) {
 
-        // Handle click on non-parent menu items
-        $menu.on('click', '.ntr-nav-item:not(.menu-item-has-children) a', function(event) {
-            if (typeof elementorProFrontend !== 'undefined' && 
-                elementorProFrontend.modules && 
-                elementorProFrontend.modules.popup) {
-                elementorProFrontend.modules.popup.closePopup({}, event);
-            }
-        });
+    var menu = $scope.find('ul.menu');
+    var parents = menu.find('li.menu-item-has-children');
+    var submenus = parents.find('ul.sub-menu');
+    var currInd;
 
-        // Setup parent menu items
-        if ($parents.length) {
-            var $links = $parents.find('> a');
-            
-            // Add indicators to parent menu items
-            $links.each(function() {
-                var $indicator = $('<span>', {
-                    'class': 'eb-accordion-wp-menu-indicator',
-                    'text': ' +'
-                });
-                $(this).append($indicator);
-            });
-
-            // Handle click on parent menu items
-            $links.on('click', function(e) {
-                e.preventDefault();
-                var $currentLink = $(this);
-                var $currentSubmenu = $currentLink.next('.sub-menu');
-                var $currentIndicator = $currentLink.find('.eb-accordion-wp-menu-indicator');
-
-                // Close other submenus
-                $submenus.not($currentSubmenu).slideUp();
-                $links.not($currentLink).find('.eb-accordion-wp-menu-indicator').text(' +');
-
-                // Toggle current submenu
-                $currentSubmenu.stop(true, true).slideToggle();
-                $currentIndicator.text($currentIndicator.text().trim() === '+' ? ' -' : ' +');
-            });
-        }
-    };
-
-    // Initialize widget using ElementBits system
-    $(window).on('elementor/frontend/init', function() {
-        if (typeof ElementBits !== 'undefined') {
-            ElementBits.initWidget('eb-accordion-menu.default', ebAccordionMenuWidget);
-        } else {
-            console.warn('ElementBits core not loaded');
-            elementorFrontend.hooks.addAction('frontend/element_ready/eb-accordion-menu.default', ebAccordionMenuWidget);
-        }
+    menu.on('click', '.ntr-nav-item:not(.menu-item-has-children) a', function(event) {
+      elementorProFrontend.modules.popup.closePopup( {}, event);
     });
 
-})(jQuery);
+    function toggleInd(val) {
+      return val === '+' ? '-' : '+';
+    }
+
+    function currInd(target) {
+      return $(target).find('.eb-accordion-wp-menu-indicator').text();
+    }
+
+    if(parents.length) {
+      var links = parents.find('> a');
+      var indEl = document.createElement('span');
+      indEl.classList.add('eb-accordion-wp-menu-indicator');
+      indEl.innerHTML = ' +';
+      links.append(indEl);
+      links.on('click', function(e) {
+        console.log(e)
+        e.preventDefault();
+        var curr = $(e.currentTarget).next('.sub-menu');
+        submenus.not(curr).slideUp();
+        curr.stop(1,1).slideToggle();
+        links.not(e.currentTarget).find('.eb-accordion-wp-menu-indicator').text(' +');
+        var ind = $(e.currentTarget).find('.eb-accordion-wp-menu-indicator');
+        ind.text(ind.text() == ' +' ? ' -' : ' +');
+      });
+    }
+  };
+
+  // Make sure you run this code under Elementor.
+  $(window).on( 'elementor/frontend/init', function() {
+    elementorFrontend.hooks.addAction( 'frontend/element_ready/eb-accordion-wp-menu.default', widget );
+  } );
+} )( jQuery );
